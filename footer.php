886a7fb80a48622d9a5c976cd5bc6eba9cd213ad
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-06-13 16:39:17 +0700

    khởi tạo project

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer id="pod-footer" class="uk-section">
+    <div class="uk-container">
+        <div class="uk-child-width-1-5@m" uk-grid>
+            <div>
+                <figure>
+                    <a href="#"><img src="imgs/logo.png" alt=""></a>
+                </figure>
+            </div>
+            <div>
+                <h4>Contact us</h4>
+            </div>
+        </div>
+    </div>
+</footer>
+</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php $title = "Trang chủ" ?>
+<?php include "header.php"; ?>
+
+
+<?php include "footer.php"; ?>
