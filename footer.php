016ed43cd8f363c1d3b868117729a03f020a2286
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-06-14 11:32:41 +0700

    update Get The App

--- footer.php
+++ footer.php
@@ -7,7 +7,39 @@
                 </figure>
             </div>
             <div>
-                <h4>Contact us</h4>
+                <h4 class="pod-footer-title">Contact us</h4>
+                <ul class="uk-list pod-footer-list">
+                    <li>support@podez.com</li>
+                    <li>+123 456 789</li>
+                </ul>
+            </div>
+            <div>
+                <h4 class="pod-footer-title">Company</h4>
+                <ul class="uk-list pod-footer-list">
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Meet the team</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4 class="pod-footer-title">Support</h4>
+                <ul class="uk-list pod-footer-list">
+                    <li><a href="#">Help Center</a></li>
+                    <li><a href="#">How it work</a></li>
+                    <li><a href="#">Community</a></li>
+                    <li><a href="#">Terms of use</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4 class="pod-footer-title">Follow us</h4>
+                <div class="pod-footer-social">
+                    <a href="" class="uk-icon-button" uk-icon="twitter"></a>
+                    <a href="" class="uk-icon-button" uk-icon="facebook"></a>
+                    <a href="" class="uk-icon-button" uk-icon="google-plus"></a>
+                    <a href="" class="uk-icon-button" uk-icon="linkedin"></a>
+                </div>
             </div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php $title = "Trang chủ" ?>
 <?php include "header.php"; ?>
 
-
+<div class="uk-section">
+    <div class="uk-container">
+        <div class="pod-block-header uk-text-center">
+            <span class="uk-text-uppercase">products</span>
+            <h2>Featured Products</h2>
+        </div>
+    </div>
+</div>
+<div id="pod-block-getapp" class="uk-section" data-src="imgs/bg-getapp.jpg" uk-img>
+    <div class="uk-container">
+        <div class="uk-grid-medium" uk-grid>
+            <div class="uk-width-expand">
+                <div class="pod-box-img uk-box-shadow-medium">
+                    <img src="imgs/Orderdetail.jpg" alt="">
+                </div>
+            </div>
+            <div class="uk-width-1-3@m uk-flex-first@m">
+                <div class="pod-block-header">
+                    <span class="uk-text-uppercase">install now</span>
+                    <h2>Get The App</h2>
+                </div>
+                <p class="pod-desc">Get the latest resources for installing, and updating PodEz. Select your device platform and Use Our app and Enjoy Your Life.</p>
+                <div class="uk-margin-medium">
+                    <a href="#" class="uk-button uk-button-secondary uk-button-large pod-install"><span uk-icon="download"></span> Install here</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include "footer.php"; ?>
