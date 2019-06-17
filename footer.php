8d44fee1173cc4c432407b70e128386e89a9b78e
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-06-17 16:51:26 +0700

    hoàn thành project

--- footer.php
+++ footer.php
@@ -6,14 +6,14 @@
                     <a href="."><img src="imgs/logo.png" alt=""></a>
                 </figure>
             </div>
-            <div>
+            <div class="uk-visible@s">
                 <h4 class="pod-footer-title">Contact us</h4>
                 <ul class="uk-list pod-footer-list">
                     <li>support@podez.com</li>
                     <li>+123 456 789</li>
                 </ul>
             </div>
-            <div>
+            <div class="uk-visible@s">
                 <h4 class="pod-footer-title">Company</h4>
                 <ul class="uk-list pod-footer-list">
                     <li><a href="#">About us</a></li>
@@ -23,7 +23,7 @@
                     <li><a href="#">Privacy Policy</a></li>
                 </ul>
             </div>
-            <div>
+            <div class="uk-visible@s">
                 <h4 class="pod-footer-title">Support</h4>
                 <ul class="uk-list pod-footer-list">
                     <li><a href="#">Help Center</a></li>
@@ -32,7 +32,7 @@
                     <li><a href="#">Terms of use</a></li>
                 </ul>
             </div>
-            <div>
+            <div class="uk-visible@s">
                 <h4 class="pod-footer-title">Follow us</h4>
                 <div class="pod-footer-social">
                     <a href="" class="uk-icon-button" uk-icon="twitter"></a>
@@ -40,6 +40,53 @@
                     <a href="" class="uk-icon-button" uk-icon="google-plus"></a>
                     <a href="" class="uk-icon-button" uk-icon="linkedin"></a>
                 </div>
+            </div>
+            <div class="uk-hidden@s">
+                <ul class="pod-menu-bottom-mobile" uk-accordion="multiple: true">
+                    <li>
+                        <a class="uk-accordion-title" href="#">Contact us</a>
+                        <div class="uk-accordion-content">
+                            <ul class="uk-list pod-footer-list">
+                                <li>support@podez.com</li>
+                                <li>+123 456 789</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="uk-accordion-title" href="#">Company</a>
+                        <div class="uk-accordion-content">
+                            <ul class="uk-list pod-footer-list">
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Meet the team</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a class="uk-accordion-title" href="#">Support</a>
+                        <div class="uk-accordion-content">
+                            <ul class="uk-list pod-footer-list">
+                                <li><a href="#">Help Center</a></li>
+                                <li><a href="#">How it work</a></li>
+                                <li><a href="#">Community</a></li>
+                                <li><a href="#">Terms of use</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="uk-open">
+                        <a class="uk-accordion-title" href="#">Follow us</a>
+                        <div class="uk-accordion-content">
+                            <div class="pod-footer-social">
+                                <a href="" class="uk-icon-button" uk-icon="twitter"></a>
+                                <a href="" class="uk-icon-button" uk-icon="facebook"></a>
+                                <a href="" class="uk-icon-button" uk-icon="google-plus"></a>
+                                <a href="" class="uk-icon-button" uk-icon="linkedin"></a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
